4a8f1ad795a3a5921253520360243167232495bb
Arie Milner <arie.milner@hey.com>
2023-02-15 08:28:38 -0800

    Update liveview to 18.3 and add buttons on feed page for logging exercise/nutrition

--- lib/open_book_web/live/feed_live.ex
+++ lib/open_book_web/live/feed_live.ex
@@ -4,7 +4,10 @@ defmodule OpenBookWeb.FeedLive do
   """
   use OpenBookWeb, :live_view
 
+  import Phoenix.Component
+
   alias OpenBook.LittleLogger, as: LL
+  alias OpenBookWeb.JournalLive
 
   def mount(_params, %{"user_id" => user_id}, socket) do
     LL.metadata_add_current_user_id(user_id)
@@ -17,8 +20,45 @@ defmodule OpenBookWeb.FeedLive do
   def render(assigns) do
     ~H"""
     Feed Page.
+
+    <.new_entry_footer />
     """
   end
 
   ## Handle Events
+
+  # Private
+
+  ## Markdown
+
+  defp new_entry_footer(assigns) do
+    ~H"""
+    <div class="new_entry_footer">
+      <p class="buttons is-right pb-4 pr-4 are-small">
+        <.link
+          navigate={Routes.live_path(OpenBookWeb.Endpoint, JournalLive)}
+          class="button is-rounded is-outlined is-link"
+        >
+          <span class="icon">
+            <i class="fas fa-utensils"></i>
+          </span>
+          <span>
+            Log Nutrition
+          </span>
+        </.link>
+        <.link
+          navigate={Routes.live_path(OpenBookWeb.Endpoint, JournalLive)}
+          class="button is-rounded is-link"
+        >
+          <span class="icon">
+            <i class="fas fa-dumbbell"></i>
+          </span>
+          <span>
+            Log Workout
+          </span>
+        </.link>
+      </p>
+    </div>
+    """
+  end
 end
