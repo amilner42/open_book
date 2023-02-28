7a55ea9f7d2c47ba9bf44c9998814de46e48801b
Arie Milner <amilner42@gmail.com>
2023-02-28 13:32:33 -0800

    Doctest class_list view_util

--- lib/open_book_web/view_utils.ex
+++ lib/open_book_web/view_utils.ex
@@ -6,7 +6,13 @@ defmodule OpenBookWeb.ViewUtils do
   @doc """
   Conditionally add classes.
 
-  Eg. `css_class = class_list("class-I-always-want", {"will-add": true, "wont-add": false})`
+  ## Examples:
+
+    iex> OpenBookWeb.ViewUtils.class_list("always-class", %{})
+    "always-class"
+
+    iex> OpenBookWeb.ViewUtils.class_list("always-class", %{"also-this-class": true, "but-not-this-class": false})
+    "always-class also-this-class"
   """
   def class_list(base_class, boolean_by_class_name) do
     Enum.reduce(boolean_by_class_name, base_class, fn {class_name, should_include}, acc ->
