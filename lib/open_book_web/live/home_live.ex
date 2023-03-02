a42f9f0a807eca91a53669efa55a18aedace9f1c
Arie Milner <amilner42@gmail.com>
2023-03-02 08:58:36 -0800

    Create markdown for sub menu bar and do basics of styling

--- lib/open_book_web/live/home_live.ex
+++ lib/open_book_web/live/home_live.ex
@@ -13,8 +13,7 @@ defmodule OpenBookWeb.HomeLive do
   alias OpenBook.Fitness
   alias OpenBook.LittleLogger, as: LL
   alias OpenBook.Share
-  alias OpenBookWeb.ExerciseLogLive
-  alias OpenBookWeb.NutritionLogLive
+  alias OpenBookWeb.Markdowns.FooterMenuBar
   alias OpenBookWeb.ViewUtils
 
   @top_bar_stats_tab "stats"
@@ -136,9 +135,8 @@ defmodule OpenBookWeb.HomeLive do
   def render(assigns) do
     ~H"""
     <%= unless @page_loading do %>
-    <.top_bar_nav selected_tab={@selected_top_bar_tab}/>
 
-    <div class="pl-5 pr-5">
+    <div class="p-5">
       <%= case @selected_top_bar_tab do %>
       <% "stats" -> %> <% # @top_bar_stats_tab %>
         <div class="has-text-centered">
@@ -295,7 +293,9 @@ defmodule OpenBookWeb.HomeLive do
       <% end %>
     </div>
 
-    <.new_entry_footer />
+    <div class="pt-6"> </div>
+
+    <FooterMenuBar.footer_menu_bar />
     <% end %>
     """
   end
@@ -399,80 +399,5 @@ defmodule OpenBookWeb.HomeLive do
         friend_exercise_summaries: friend_exercise_summaries
       }
     end
-  end
-
-  ## Markdown
-
-  defp top_bar_nav(assigns) do
-    ~H"""
-    <div class="buttons top-bar has-addons is-centered pt-4 pb-4 mb-0 button-navbar">
-      <button
-        class={ViewUtils.class_list("button", %{"is-selected": @selected_tab == "stats" })}
-        style="width: 110px"
-        phx-click="top_level_navigate"
-        phx-value-top_bar_tab="stats"
-      >
-        <span class="icon">
-          <i class="fas fa-trophy"></i>
-        </span>
-        <span>Stats</span>
-      </button>
-
-      <button
-        class={ViewUtils.class_list("button", %{"is-selected": @selected_tab == "book" })}
-        style="width: 110px"
-        phx-click="top_level_navigate"
-        phx-value-top_bar_tab="book"
-      >
-        <span class="icon">
-          <i class="fas fa-book"></i>
-        </span>
-        <span>Book</span>
-      </button>
-
-      <button
-        class={ViewUtils.class_list("button", %{"is-selected": @selected_tab == "friends" })}
-        style="width: 110px"
-        phx-click="top_level_navigate"
-        phx-value-top_bar_tab="friends"
-      >
-        <span class="icon">
-          <i class="fas fa-user-friends"></i>
-        </span>
-        <span>Friends</span>
-      </button>
-    </div>
-    """
-  end
-
-  defp new_entry_footer(assigns) do
-    ~H"""
-    <div class="new_entry_footer">
-      <p class="buttons is-right pb-4 pr-4 ">
-        <.link
-          navigate={Routes.live_path(OpenBookWeb.Endpoint, NutritionLogLive)}
-          class="button is-light has_border_grey has_text_dark_blue b-0"
-        >
-          <span class="icon has_text_dark_blue">
-            <i class="fas fa-utensils mr-0"></i>
-          </span>
-          <span class="has_text_dark_blue">
-            log nutrition
-          </span>
-        </.link>
-        <.link
-          navigate={Routes.live_path(OpenBookWeb.Endpoint, ExerciseLogLive)}
-          class="button is-light has_border_grey has_text_dark_blue b-0"
-        >
-          <span class="icon has_text_dark_blue">
-            <i class="fas fa-dumbbell"></i>
-          </span>
-          <span>
-            record workout
-          </span>
-        </.link>
-      </p>
-    </div>
-    """
   end
 end
